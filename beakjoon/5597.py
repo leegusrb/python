af5320986e4463ba977cba4ea040b99a215ed751
Lee Hyeon Gyu <jeffsep24@gmail.com>
2022-10-31 23:28:41 +0900

    baekjoon 5597

---
+++ beakjoon/5597.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+li = [0 for i in range(30)]
+
+for i in range(28):
+    li[int(input()) - 1] = 1
+
+for i in range(30):
+    if li[i] == 0:
+        print(i + 1)

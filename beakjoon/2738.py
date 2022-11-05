4373b30b6d9ed11ae738a02080a490d7f0dfac02
Lee Hyeon Gyu <jeffsep24@gmail.com>
2022-11-05 23:25:21 +0900

    baekjoon 15964

---
+++ beakjoon/2738.py
@@ -0,0 +1,15 @@
+N, M = map(int, input().split())
+
+A = list()
+B = list()
+
+for i in range(N):
+    A.append(list(map(int, input().split())))
+
+for i in range(N):
+    B.append(list(map(int, input().split())))
+
+for i in range(N):
+    for j in range(M):
+        print(A[i][j] + B[i][j], end=" ")
+    print()
